ec5ca48f913556d9e2b7fa28a2ea788f0a38d6b4
katherine-meezan <katherinemeezan@gmail.com>
2025-11-30 17:08:10 -0800

    Updated motor gains

--- Lab0x05/on_board/main.py
+++ Lab0x05/on_board/main.py
@@ -67,9 +67,9 @@ BAT_READ = pyb.ADC(PC2)
 
 # CONTROLLER SETPOINT IS IN MM/S
 cl_ctrl_mot_left = CLMotorController(0, 0, 0, Kp=.5, Ki=5, min_sat=-100, max_sat=100, t_init=0,
-                                     v_nom=2.878, threshold=1.439, K3=.0209)
+                                     v_nom=2.878, threshold=1.439, K3=.0669)
 cl_ctrl_mot_right = CLMotorController(0, 0, 0, Kp=.5, Ki=5, min_sat=-100, max_sat=100, t_init=0,
-                                      v_nom=2.878, threshold=1.439, K3=.0216)
+                                      v_nom=2.878, threshold=1.439, K3=.0684)
 
 ir_ch1 = IR_sensor(Pin(Pin.cpu.C3, mode=Pin.ANALOG))
 ir_ch3 = IR_sensor(Pin(Pin.cpu.A4, mode=Pin.ANALOG))
