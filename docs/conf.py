1952b20c7300d875c1845f20d2dc0c2d6a8412cc
katherine-meezan <katherinemeezan@gmail.com>
2025-12-10 21:00:26 -0800

    Adding stuff for repo page

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ME 405 Final Project'
+copyright = '2025, Katherine Meezan, Michael Habib, Zachery Boyer'
+author = 'Katherine Meezan, Michael Habib, Zachery Boyer'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
