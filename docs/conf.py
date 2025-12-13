7105a98375dd31ea5622ae62f3664f1800040025
katherine-meezan <katherinemeezan@gmail.com>
2025-12-12 17:06:38 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ autodoc_default_options = {
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ["utime", "machine", "pyb", "micropython"]
+autodoc_mock_imports = ["utime", "machine", "pyb", "micropython", "cqueue"]
 
 
 
